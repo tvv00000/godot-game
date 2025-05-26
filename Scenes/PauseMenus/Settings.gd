2228b123601046332387dd8dd52c90d656538b33
taaavi1 <tvv@tlu.ee>
2025-05-26 09:26:36 +0300

    fast ui fix 2

--- Scenes/PauseMenus/Settings.gd
+++ Scenes/PauseMenus/Settings.gd
@@ -1,4 +1,22 @@
 extends Control
+@onready var inventory_menu = $"../PauseMenu"
 
 func _ready() -> void:
 	hide()
+
+
+func _on_resume_pressed() -> void:
+	hide()
+	inventory_menu.show()
+
+
+func _on_graafika_pressed() -> void:
+	OS.alert('Pole veel', 'alert')
+
+
+func _on_heli_pressed() -> void:
+	OS.alert('Pole veel', 'alert')
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
