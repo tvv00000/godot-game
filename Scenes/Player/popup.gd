14dca52ce49537547566a8dde6f72bd2d990889e
Dylan Michael Jones <dylanmj@tlu.ee>
2025-06-10 11:01:43 +0300

    Item pickup popup

---
+++ Scenes/Player/popup.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+@onready var pickup_label = $PickupLabel
+var camera: Camera3D
+
+
+func item_pickup(position_3d: Vector3, message: String):
+	var screen_pos = Global.player.get_node("Pivot_Camera/Camera3D").unproject_position(position_3d)
+	pickup_label.text = message
+	pickup_label.global_position = screen_pos + Vector2(0, -30)
+	pickup_label.visible = true	
+	pickup_label.modulate.a = 1.0
+	var tween := create_tween()
+	tween.tween_property(pickup_label, "modulate:a", 0.0, 2.0)
+	tween.tween_callback(Callable(pickup_label, "hide"))
+	print("item pickup")
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -6,3 +6,6 @@ var player: Node = null
 
 #Kasvatamine
 var planterDirtRatio: int = 0
+
+var inv_ui
+var camera
