c87c85ff2aba61ff0e8985315da758647a6e1782
fredtare <tarenomm@tlu.ee>
2025-06-19 18:32:40 +0300

    Tutorialisse lisatud ka enter peitmiseks ja taimer et men[[ ei kaoks kohe

--- Scenes/PauseMenus/TutorialMenu.gd
+++ Scenes/PauseMenus/TutorialMenu.gd
@@ -7,11 +7,20 @@ func _ready():
 
 func show_tutorial(msg: String):
 	tutorialText.text = msg
+	Global.isInteracting = true
+	await get_tree().create_timer(0.1).timeout
 	show()
+	
+
 
 func _on_tutorial_exit_btn_pressed() -> void:
-	hide()
+	closeMenu()
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("Move_Jump"):
-		hide()
+	
+	if Input.is_action_just_released("Move_Jump") or Input.is_action_pressed("UI_Interact"):
+		closeMenu()
+		
+func closeMenu():
+	Global.isInteracting = false
+	hide()
