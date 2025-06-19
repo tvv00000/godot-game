567b2969416a08a7d5792025894066b026b0b195
fredtare <tarenomm@tlu.ee>
2025-06-19 11:57:04 +0300

    Jump fix, again

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -65,12 +65,12 @@ func _physics_process(delta: float) -> void:
 		# Add the gravity.
 		if not is_on_floor():
 			velocity += get_gravity() * delta
-			print("not on floor")
 		# Handle jump.
 		if is_on_floor():
 			if Input.is_action_just_pressed("Move_Jump") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
 				animation.play("Jump")
-				print("floor")
+
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
