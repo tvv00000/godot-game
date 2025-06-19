aa39b59358a3396d699b162ef426bd1dd8dc215c
Dylan Michael Jones <dylanmj@tlu.ee>
2025-06-19 11:46:54 +0300

    ui sounds

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -65,11 +65,12 @@ func _physics_process(delta: float) -> void:
 		# Add the gravity.
 		if not is_on_floor():
 			velocity += get_gravity() * delta
-
+			print("not on floor")
 		# Handle jump.
-		if Input.is_action_just_pressed("Move_Jump") and is_on_floor():
-			animation.play("Jump")
-			
+		if is_on_floor():
+			if Input.is_action_just_pressed("Move_Jump") and is_on_floor():
+				animation.play("Jump")
+				print("floor")
 
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
